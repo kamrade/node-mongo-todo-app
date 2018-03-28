0e5aeaaa4aff44de2ce131b8e899437eac65ad7f
Denis Mikhailov <denis.michailov@gmail.com>
2018-03-28 17:05:15 +1000

    Test POST /todos route

---
+++ data/paths.js
@@ -0,0 +1,5 @@
+const _paths = {
+  addTodo: '/v1/todos'
+};
+
+module.exports = _paths;
--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const { mongoose } = require('./db/mongoose');
 const { Todo }     = require('./models/todo');
 const { User }     = require('./models/user');
 
+const _paths = require('./data/paths');
 const PORT = 3200;
 const app = express();
 app.use(bodyParser.json());
 
-
+console.log(_paths);
 
 // API --------------------------------
 
-app.post('/v1/todos', (req, res) => {
+app.post(_paths.addTodo, (req, res) => {
   let todo = new Todo({
     text: req.body.text
   });
@@ -35,3 +36,5 @@ app.post('/v1/todos', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Started server on port ${PORT}`);
 });
+
+module.exports = { app };
