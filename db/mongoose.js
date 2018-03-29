3d0d995c7beccb7be54f274066ff0bead617099d
Denis Mikhailov <denis.michailov@gmail.com>
2018-03-29 16:48:37 +1000

    setup different environments

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,21 @@
 let mongoose = require('mongoose');
 
-const env    = process.env.NODE_ENV || 'dev';
-const dbName = env === 'dev' ? 'Todos' : 'node-mongo-tutorial-app';
-const url    = env === 'dev' ? 'mongodb://localhost:27017' : 'mongodb://kamrade:kamrade123@ds127589.mlab.com:27589';
+const env = process.env.NODE_ENV || 'dev';
+let dbName, url;
+
+if (env === 'dev') {
+  dbName = 'Todos';
+  url = 'mongodb://localhost:27017';
+} else if (env === 'test') {
+  dbName = 'TestTodos';
+  url = 'mongodb://localhost:27017';
+} else if (env === 'production') {
+  dbName = 'node-mongo-tutorial-app';
+  url = 'mongodb://kamrade:kamrade123@ds127589.mlab.com:27589';
+} else {
+  dbName = 'Todos';
+  url = 'mongodb://localhost:27017';
+}
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`${url}/${dbName}`);
